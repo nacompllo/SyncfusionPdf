c9881b2c2546710828294f4fbdc0b94056ee98ac
Nacompllo <lilloserg96@outlook.com>
2021-11-01 18:57:17 +0100

    Add project files.

---
+++ SyncfusionPdf/SyncfusionPdf/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Xamarin.Forms;
+using System.IO;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Drawing;
+using SyncfusionPdf.Services;
+
+namespace SyncfusionPdf
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly ISave _save;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            _save = DependencyService.Get<ISave>();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            // Create a new PDF document
+            PdfDocument document = new PdfDocument();
+
+            //Add a page to the document
+            PdfPage page = document.Pages.Add();
+
+            //Create PDF graphics for the page
+            PdfGraphics graphics = page.Graphics;
+
+            //Set the standard font
+            PdfFont font = new PdfStandardFont(PdfFontFamily.Helvetica, 14, PdfFontStyle.Bold);
+            PdfFont fontAutor = new PdfStandardFont(PdfFontFamily.Helvetica, 12);
+
+            //Draw the text
+            graphics.DrawString("Test", font, PdfBrushes.Black, new PointF(0, 0));
+
+            //Save the document to the stream
+            MemoryStream stream = new MemoryStream();
+            document.Save(stream);
+
+            //Close the document
+            document.Close(true);
+
+            //Save the stream as a file in the device and invoke it for viewing
+            _save.SaveAndView("Output.pdf", "application/pdf", stream);
+        }
+    }
+}
